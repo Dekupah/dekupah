c7675c92e9bb53212f05dbcf66d5b3d1a8e7a900
Cristiano Reis <117485646+Akumanoir@users.noreply.github.com>
2023-12-21 18:52:48 -0300

    changed section tags on projectWrapperComponents for article tags and ghanged the component name for ArticleComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ProjectWrapperComponent as ProjectWrapper } from "./components/projectWrapperComponent/index"
+import { ArticleComponent as Article } from "./components/Article/index"
 import Logo from "./assets/dekupah-logo.png"
 
 export default function App() {
@@ -7,13 +7,15 @@ export default function App() {
     <>
       <header className="w-full">
         <section className="max-w-5xl m-auto">
-          <img src={Logo} alt="" className="object-cover" />
+          <h1>
+            <img src={Logo} alt="Dekupah" data-src-webp="" className="object-cover" />
+          </h1>
         </section>
       </header>
       <main className="max-w-5xl m-auto flex flex-col gap-3">
           {/* Sessão de sobre mim */}
           <section className="flex flex-col gap-3">
-            <h1 className="text-3xl font-bold uppercase" style={{color: "#F27C38"}}>Quem nós somos?</h1>
+            <h2 className="text-3xl font-bold uppercase" style={{color: "#F27C38"}}>Quem nós somos?</h2>
             <p className="text-lg" style={{color: "#595656"}}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vero consequuntur nobis quas cumque labore asperiores sequi optio iure, rerum adipisci, tempora dignissimos architecto natus ipsam voluptatum? Sed, quas officia. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, error quia nobis nisi facere sed qui quod quam dolor in voluptatem, velit ex incidunt illo a ea harum numquam perspiciatis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quas nulla illo. Eum, in! Reiciendis tempora itaque commodi unde sit molestiae doloremque assumenda ea excepturi, iusto, harum, magni ad ipsa! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit quas rerum culpa, nemo debitis, totam corporis neque dicta labore eveniet illo est cupiditate reiciendis assumenda quos repellat itaque vel error.
             </p>
@@ -21,32 +23,32 @@ export default function App() {
           {/* Parte de projetos */}
           <section className="flex flex-col gap-3">
             <section>
-              <h1 className="text-3xl font-bold uppercase" style={{color: "#F27C38"}}>Nossos Projetos</h1>
+              <h2 className="text-3xl font-bold uppercase" style={{color: "#F27C38"}}>Nossos Projetos</h2>
             </section>
             <section>
               {/*--------------------------------------------------------------*/}
-              <ProjectWrapper
+              <Article
                 title = "Projeto 1"
                 text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
                 imageUrl="https://randomuser.me/api/portraits/women/50.jpg"
                 index={1}
               />
               {/*--------------------------------------------------------------*/}
-              <ProjectWrapper
+              <Article
                 title = "Projeto 2"
                 text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
                 imageUrl="https://randomuser.me/api/portraits/women/50.jpg"
                 index={2}
               />
               {/*--------------------------------------------------------------*/}
-              <ProjectWrapper
+              <Article
                 title = "Projeto 3"
                 text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
                 imageUrl="https://randomuser.me/api/portraits/women/50.jpg"
                 index={3}
               />
               {/*--------------------------------------------------------------*/}
-              <ProjectWrapper
+              <Article
                 title = "Projeto 4"
                 text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
                 imageUrl="https://randomuser.me/api/portraits/women/50.jpg"
--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,20 +1,20 @@
-interface projectWrapperComponentProps {
+interface ArticleComponentProps {
   title: string;
   text: string;
   imageUrl: string;
   index: number;
 }
 
-export function ProjectWrapperComponent({ title, text, imageUrl, index }: projectWrapperComponentProps) {
+export function ArticleComponent({ title, text, imageUrl, index }: ArticleComponentProps) {
 
   return (
     <>
       {index % 2 != 0 ? (
-        <section className="flex w-full justify-between py-3 h-52 overflow-hidden">
+        <article className="flex w-full justify-between py-3 h-52 overflow-hidden">
           <section className="max-w-[60%]">
-            <h2 className="text-xl font-bold uppercase" style={{ color: "#595656" }}>
+            <h1 className="text-xl font-bold uppercase" style={{ color: "#595656" }}>
               {title}
-            </h2>
+            </h1>
             <p className="text-lg" style={{ color: "#595656" }}>
               {text}
             </p>
@@ -23,22 +23,22 @@ export function ProjectWrapperComponent({ title, text, imageUrl, index }: projec
             <div className="w-0 h-0 absolute" style={{ borderRight: "50px solid transparent", borderTop: "200px solid white" }}></div>
             <img src={imageUrl} alt="" className="object-cover h-full w-full" />
           </section>
-        </section>
+        </article>
       ) : (
-        <section className="flex w-full justify-between py-3 h-52 overflow-hidden">
+        <article className="flex w-full justify-between py-3 h-52 overflow-hidden">
           <section className="h-full w-[40%] relative">
             <div className="w-0 h-0 absolute right-0" style={{ borderLeft: "50px solid transparent", borderBottom: "200px solid white" }}></div>
             <img src={imageUrl} alt="" className="object-cover h-full w-full" />
           </section>
           <section className="max-w-[60%] ml-2">
-            <h2 className="text-xl font-bold uppercase" style={{ color: "#595656" }}>
+            <h1 className="text-xl font-bold uppercase" style={{ color: "#595656" }}>
               {title}
-            </h2>
+            </h1>
             <p className="text-lg" style={{ color: "#595656" }}>
               {text}
             </p>
           </section>
-        </section>
+        </article>
       )}
     </>
   );
