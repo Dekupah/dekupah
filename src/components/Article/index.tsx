d2054e08567e6bfffd7d7410d293fd621b5ff15f
Cristiano Reis <117485646+Akumanoir@users.noreply.github.com>
2023-12-29 16:43:59 -0300

    added minor change to image hover

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -8,12 +8,12 @@ interface ArticleComponentProps {
 }
 
 export function ArticleComponent({ title, text, imageUrl, index }: ArticleComponentProps) {
-// className="max-w-[60%]"
-  const [_isHovering, setIsHovering] = useState(false);
+// className="max-w-[60%]" onMouseMove={handleWindowMouseMove}
+  const [isHovering, setIsHovering] = useState(false);
 
-  const [coords, setCoords] = useState(Array(2).fill(null));
+  // const [coords, setCoords] = useState(Array(2).fill(null));
 
-  const [width, setWidth] = useState(1600)
+  // const [width, setWidth] = useState(1600)
   
   const handleMouseOver = () => {
     setIsHovering(true);
@@ -21,44 +21,62 @@ export function ArticleComponent({ title, text, imageUrl, index }: ArticleCompon
 
   const handleMouseOut = () => {
     setIsHovering(false);
-    setWidth(1600)
+    // setWidth(1600)
   };
 
-  const handleWindowMouseMove = (event:any) => {
-    let nextCoord = coords.slice()
+  // altura 184
 
-    if (nextCoord[0] == null) {
-      nextCoord[0] = event.clientX
-    }
-
-    nextCoord[1] = event.clientX
-
-    if (index % 2 != 0) {
-      if (nextCoord[1] < nextCoord[0]) {
-        setWidth(width + 10)
-        nextCoord[0] = nextCoord[1]
-      } else {
-        setWidth(width -10)
-        nextCoord[0] = nextCoord[1]
-      }
+  const width = () => {
+    if (isHovering) {
+      return {width: "1000px", height: "280px"}
     } else {
-      if (nextCoord[1] > nextCoord[0]) {
-        setWidth(width + 10)
-        nextCoord[0] = nextCoord[1]
-      } else {
-        setWidth(width -10)
-        nextCoord[0] = nextCoord[1]
-      }
+      return {width: "1600px", height: "184px"}
     }
+  }
+
+  const height = () => {
+    if (isHovering) {
+      return {height: "210px", transform: "translateY(-1rem)"}
+    } else {
+      return {height: "184px", transform: "translateY(0)"}
+    }
+  }
+
+  // const handleWindowMouseMove = (event:any) => {
+  //   let nextCoord = coords.slice()
+
+  //   if (nextCoord[0] == null) {
+  //     nextCoord[0] = event.clientX
+  //   }
+
+  //   nextCoord[1] = event.clientX
+
+  //   if (index % 2 != 0) {
+  //     if (nextCoord[1] < nextCoord[0]) {
+  //       setWidth(width + 10)
+  //       nextCoord[0] = nextCoord[1]
+  //     } else {
+  //       setWidth(width -10)
+  //       nextCoord[0] = nextCoord[1]
+  //     }
+  //   } else {
+  //     if (nextCoord[1] > nextCoord[0]) {
+  //       setWidth(width + 10)
+  //       nextCoord[0] = nextCoord[1]
+  //     } else {
+  //       setWidth(width -10)
+  //       nextCoord[0] = nextCoord[1]
+  //     }
+  //   }
       
-    setCoords(nextCoord)
-  };
+  //   setCoords(nextCoord)
+  // };
 
   return (
     <>
       {index % 2 != 0 ? (
-        <article className="flex w-full justify-between py-3 h-52 overflow-hidden transition duration-300">
-          <section className="min-w-[40%]" style={{width: `${width}px`, maxWidth: "60%"}}>
+        <article className="flex w-full justify-between py-3 overflow-hidden transition-all duration-300" style={height()}>
+          <section className="min-w-[40%] transition-all duration-300" style={{...width(), maxWidth: "60%"}}>
             <h1 className="text-xl font-bold uppercase" style={{ color: "#595656" }}>
               {title}
             </h1>
@@ -66,18 +84,18 @@ export function ArticleComponent({ title, text, imageUrl, index }: ArticleCompon
               {text}
             </p>
           </section>
-          <section className="h-full w-full relative cursor-pointer" onMouseMove={handleWindowMouseMove} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-            <div className="w-0 h-0 absolute" style={{ borderRight: "50px solid transparent", borderTop: "200px solid white" }}></div>
+          <section className="h-full w-full relative cursor-pointer" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+            <div className="w-0 h-0 absolute" style={{ borderRight: "50px solid transparent", borderTop: "200px solid white"}}></div>
             <img src={imageUrl} alt="" className="object-cover h-full w-full" />
           </section>
         </article>
       ) : (
-        <article className="flex w-full justify-between py-3 h-52 overflow-hidden transition duration-300">
-          <section className="h-full w-full relative cursor-pointer" onMouseMove={handleWindowMouseMove} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+        <article className="flex w-full justify-between py-3 h-52 overflow-hidden transition-all duration-300" style={height()}>
+          <section className="h-full w-full relative cursor-pointer" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
             <div className="w-0 h-0 absolute right-0" style={{ borderLeft: "50px solid transparent", borderBottom: "200px solid white" }}></div>
             <img src={imageUrl} alt="" className="object-cover h-full w-full" />
           </section>
-          <section className="min-w-[40%]" style={{width: `${width}px`, maxWidth: "60%"}}>
+          <section className="min-w-[40%] transition-all duration-300" style={{...width(), maxWidth: "60%"}}>
             <h1 className="text-xl font-bold uppercase" style={{ color: "#595656" }}>
               {title}
             </h1>
