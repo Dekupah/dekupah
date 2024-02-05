c439ee305bfc0c83179ae4bc303017c0226de064
Cristiano Reis <117485646+Akumanoir@users.noreply.github.com>
2024-02-05 11:38:10 -0300

    added snap on scroll feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,92 @@
-import { ArticleComponent as Article } from "./components/Article/index"
-import Logo from "./assets/dekupah-logo.png"
+import { ArticleComponent as Article } from "./components/Article/index";
+import Logo from "./assets/dekupah-logo.png";
 
 export default function App(): any {
-
   return (
     <>
-      <header className="w-full">
-        <section className="max-w-5xl m-auto">
-          <h1>
+      <header className="w-full bg-white h-screen" style={{ scrollSnapAlign: "start" }}>
+        <section className="max-w-5xl m-auto relative">
+          <h1 className="absolute">
             <img src={Logo} alt="Dekupah" data-src-webp="" className="object-cover" />
           </h1>
+          <h2 className="absolute top-[27rem] font-bold text-2xl" style={{ color: "#595656" }}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </h2>
         </section>
       </header>
-      <main className="max-w-5xl m-auto flex flex-col gap-3">
-          {/* Sessão de sobre mim */}
-          <section className="flex flex-col gap-3">
-            <h2 className="text-3xl font-bold uppercase" style={{color: "#F27C38"}}>Quem nós somos?</h2>
-            <p className="text-lg" style={{color: "#595656"}}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vero consequuntur nobis quas cumque labore asperiores sequi optio iure, rerum adipisci, tempora dignissimos architecto natus ipsam voluptatum? Sed, quas officia. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, error quia nobis nisi facere sed qui quod quam dolor in voluptatem, velit ex incidunt illo a ea harum numquam perspiciatis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quas nulla illo. Eum, in! Reiciendis tempora itaque commodi unde sit molestiae doloremque assumenda ea excepturi, iusto, harum, magni ad ipsa! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit quas rerum culpa, nemo debitis, totam corporis neque dicta labore eveniet illo est cupiditate reiciendis assumenda quos repellat itaque vel error.
+      <main className="flex flex-col gap-3">
+        {/* Sessão de sobre mim */}
+        <div>
+          <section className="max-w-5xl m-auto flex flex-col gap-3 h-screen py-24" style={{ scrollSnapAlign: "start" }}>
+            <h2 className="text-3xl font-bold uppercase" style={{ color: "#F27C38" }}>
+              Quem nós somos?
+            </h2>
+            <p className="text-lg" style={{ color: "#595656" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vero consequuntur nobis quas cumque labore asperiores sequi optio iure, rerum adipisci, tempora dignissimos architecto natus ipsam voluptatum? Sed, quas officia. Lorem
+              ipsum dolor, sit amet consectetur adipisicing elit. Totam, error quia nobis nisi facere sed qui quod quam dolor in voluptatem, velit ex incidunt illo a ea harum numquam perspiciatis. Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Nostrum quas nulla illo. Eum, in! Reiciendis tempora itaque commodi unde sit molestiae doloremque assumenda ea excepturi, iusto, harum, magni ad ipsa! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit quas rerum
+              culpa, nemo debitis, totam corporis neque dicta labore eveniet illo est cupiditate reiciendis assumenda quos repellat itaque vel error.
             </p>
           </section>
-          {/* Parte de projetos */}
-          <section className="flex flex-col gap-3">
+        </div>
+        {/* Parte de projetos */}
+        <div className="bg-white">
+          <section className="max-w-5xl m-auto flex flex-col gap-10 pt-14 pb-24" style={{ scrollSnapAlign: "start" }}>
             <section>
-              <h2 className="text-3xl font-bold uppercase" style={{color: "#F27C38"}}>Nossos Projetos</h2>
+              <h2 className="text-3xl font-bold uppercase" style={{ color: "#F27C38" }}>
+                Nossos Projetos
+              </h2>
             </section>
             <section>
               {/*--------------------------------------------------------------*/}
               <Article
-                title = "Projeto 1"
-                text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
+                title="Projeto 1"
+                text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
                 imageUrl="https://randomuser.me/api/portraits/women/50.jpg"
                 index={1}
               />
               {/*--------------------------------------------------------------*/}
               <Article
-                title = "Projeto 2"
-                text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
+                title="Projeto 2"
+                text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
                 imageUrl="https://randomuser.me/api/portraits/women/50.jpg"
                 index={2}
               />
               {/*--------------------------------------------------------------*/}
               <Article
-                title = "Projeto 3"
-                text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
+                title="Projeto 3"
+                text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
                 imageUrl="https://randomuser.me/api/portraits/women/50.jpg"
                 index={3}
               />
               {/*--------------------------------------------------------------*/}
               <Article
-                title = "Projeto 4"
-                text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
+                title="Projeto 4"
+                text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam voluptates deleniti mollitia nobis iusto rerum voluptatum facere praesentium placeat aliquam, dolor soluta. Earum eum error sed exercitationem autem maiores at."
                 imageUrl="https://randomuser.me/api/portraits/women/50.jpg"
                 index={4}
               />
             </section>
           </section>
+        </div>
       </main>
-      <footer className="flex p-5 border-t">
+      <footer className="flex p-5 border-t" style={{ scrollSnapAlign: "start" }}>
         <section>
           <h2>Dekupah</h2>
-          <h3>
-            Entre em contato
-          </h3>
+          <h3>Entre em contato</h3>
           <ul>
-            <li><a href="#">Redes sociais 1</a></li>
-            <li><a href="#">Redes sociais 2</a></li>
-            <li><a href="#">Redes sociais 3</a></li>
+            <li>
+              <a href="#">Redes sociais 1</a>
+            </li>
+            <li>
+              <a href="#">Redes sociais 2</a>
+            </li>
+            <li>
+              <a href="#">Redes sociais 3</a>
+            </li>
           </ul>
         </section>
       </footer>
     </>
-  )
+  );
 }
--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -8,13 +8,13 @@ interface ArticleComponentProps {
 }
 
 export function ArticleComponent({ title, text, imageUrl, index }: ArticleComponentProps) {
-// className="max-w-[60%]" onMouseMove={handleWindowMouseMove}
+  // className="max-w-[60%]" onMouseMove={handleWindowMouseMove}
   const [isHovering, setIsHovering] = useState(false);
 
   // const [coords, setCoords] = useState(Array(2).fill(null));
 
   // const [width, setWidth] = useState(1600)
-  
+
   const handleMouseOver = () => {
     setIsHovering(true);
   };
@@ -28,19 +28,19 @@ export function ArticleComponent({ title, text, imageUrl, index }: ArticleCompon
 
   const width = () => {
     if (isHovering) {
-      return {width: "1000px", height: "280px"}
+      return { width: "1000px", height: "280px" };
     } else {
-      return {width: "1600px", height: "184px"}
+      return { width: "1600px", height: "184px" };
     }
-  }
+  };
 
   const height = () => {
     if (isHovering) {
-      return {height: "210px", transform: "translateY(-1rem)"}
+      return { height: "210px", transform: "translateY(-1rem)" };
     } else {
-      return {height: "184px", transform: "translateY(0)"}
+      return { height: "184px", transform: "translateY(0)" };
     }
-  }
+  };
 
   // const handleWindowMouseMove = (event:any) => {
   //   let nextCoord = coords.slice()
@@ -68,7 +68,7 @@ export function ArticleComponent({ title, text, imageUrl, index }: ArticleCompon
   //       nextCoord[0] = nextCoord[1]
   //     }
   //   }
-      
+
   //   setCoords(nextCoord)
   // };
 
@@ -76,7 +76,7 @@ export function ArticleComponent({ title, text, imageUrl, index }: ArticleCompon
     <>
       {index % 2 != 0 ? (
         <article className="flex w-full justify-between py-3 overflow-hidden transition-all duration-300" style={height()}>
-          <section className="min-w-[40%] transition-all duration-300" style={{...width(), maxWidth: "60%"}}>
+          <section className="min-w-[40%] transition-all duration-300" style={{ ...width(), maxWidth: "60%" }}>
             <h1 className="text-xl font-bold uppercase" style={{ color: "#595656" }}>
               {title}
             </h1>
@@ -85,7 +85,7 @@ export function ArticleComponent({ title, text, imageUrl, index }: ArticleCompon
             </p>
           </section>
           <section className="h-full w-full relative cursor-pointer" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-            <div className="w-0 h-0 absolute" style={{ borderRight: "50px solid transparent", borderTop: "200px solid white"}}></div>
+            <div className="w-0 h-0 absolute" style={{ borderRight: "50px solid transparent", borderTop: "200px solid white" }}></div>
             <img src={imageUrl} alt="" className="object-cover h-full w-full" />
           </section>
         </article>
@@ -95,7 +95,7 @@ export function ArticleComponent({ title, text, imageUrl, index }: ArticleCompon
             <div className="w-0 h-0 absolute right-0" style={{ borderLeft: "50px solid transparent", borderBottom: "200px solid white" }}></div>
             <img src={imageUrl} alt="" className="object-cover h-full w-full" />
           </section>
-          <section className="min-w-[40%] transition-all duration-300" style={{...width(), maxWidth: "60%"}}>
+          <section className="min-w-[40%] transition-all duration-300" style={{ ...width(), maxWidth: "60%" }}>
             <h1 className="text-xl font-bold uppercase" style={{ color: "#595656" }}>
               {title}
             </h1>
